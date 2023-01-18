0ce3de8e9f81e79ee7c7c969dde8eb4fb433d98f
aaa aaa <aaaaaa@aaas-MacBook-Pro.local>
2023-01-18 15:57:02 +0900

    [ADD]Moongose

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { createApp } = require("./app");
 const mongoose = require("mongoose");
 const express = require("express");
 const PORT = process.env.PORT;
+
 const mongoUrl =
   "mongodb+srv://root:dnjstmdgus12@mongodbtutorial.qht8epf.mongodb.net/BlogService?retryWrites=true&w=majority";
 
@@ -13,8 +14,9 @@ const server = async () => {
     const app = createApp();
     console.log("MongoDb Connected!!");
     app.use(express.json());
+
     app.listen(PORT, () => {
-      console.log(`Server Listening on port ${PORT}!!`);
+      console.log(`Server Listening on port ${PORT}`);
     });
   } catch (err) {
     console.log(err);
